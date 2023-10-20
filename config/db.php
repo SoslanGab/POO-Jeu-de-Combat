18488b12c51da5cab4dc26d2cde58035e332f7d6
SoslanGab <so.gabarayev@gmail.com>
2023-10-20 12:16:17 +0200

    db

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 
 try{
-    $db = new PDO("mysql:host=127.0.0.1;dbname=soslan_combat;charset=utf8", "soslan", "");
+    $db = new PDO("mysql:host=127.0.0.1;dbname=soslan_combat;charset=utf8", "soslan", "270769");
 }catch(PDOException $e){
     echo "Connection failed: " . $e->getMessage();
 }
